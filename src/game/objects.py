1a0e982e195064eaff39a1846414e9435b2e223b
Eltaf-azizi <iltfhuss@gmail.com>
2025-09-20 22:12:47 +0500

    working on the Pipe

--- src/game/objects.py
+++ src/game/objects.py
@@ -16,3 +16,24 @@ class Bird:
 
     def flap(self):
         self.vel = settings.FLAP_VELOCITY
+
+
+    def update(self):
+        self.vel += settings.GRAVITY
+        if self.vel > settings.MAX_DROP_SPEED:
+            self.vel = settings.MAX_DROP_SPEED
+        self.y += self.vel
+        self.rect.topleft = (self.x, int(self.y))
+
+
+
+class Pipe:
+    def __init__(self, x, gap_y, gap_size=settings.PIPE_GAP_SIZE):
+        self.x = float(x)
+        self.gap_y = int(gap_y)
+        self.gap_size = int(gap_size)
+        self.width = int(settings.PIPE_WIDTH)
+        self.top_rect = pygame.Rect(int(self.x), 0, self.width, int(self.gap_y))
+        self.bottom_rect = pygame.Rect(int(self.x), int(self.gap_y + self.gap_size),
+                                       self.width, settings.SCREEN_HEIGHT - int(self.gap_y + self.gap_size))
+        self.passed = False
