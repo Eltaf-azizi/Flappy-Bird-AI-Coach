76041cb1427b1b680a2929a4caba56575190f8a1
Eltaf-azizi <iltfhuss@gmail.com>
2025-09-21 21:32:28 +0500

    working on the class agent

--- src/ai/agent.py
+++ src/ai/agent.py
@@ -39,3 +39,28 @@ class ReplayBuffer:
                             float(reward),
                             np.array(next_state, dtype=np.float32),
                             float(done)))
+        
+    
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        s, a, r, ns, d = map(np.array, zip(*batch))
+        return s, a, r, ns, d
+
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+
+class Agent:
+    def __init__(self, state_dim=settings.STATE_SIZE, action_dim=settings.ACTION_SIZE, lr=settings.LR):
+        self.policy_net = DQN(state_dim, action_dim).to(DEVICE)
+        self.target_net = DQN(state_dim, action_dim).to(DEVICE)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.optimizer = optim.Adam(self.policy_net.parameters(), lr=lr)
+        self.replay = ReplayBuffer()
+        self.steps = 0
+        self.eps_start = 1.0
+        self.eps_end = 0.01
+        self.eps_decay = 20000  # steps to decay
+        self.gamma = settings.GAMMA
