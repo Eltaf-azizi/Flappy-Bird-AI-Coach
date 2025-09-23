d675038928fdad8d0aed75fae5502313edfda211
Eltaf-azizi <iltfhuss@gmail.com>
2025-09-23 22:19:13 +0500

    working on the stuff needed

--- src/integration/evaluate_agent.py
+++ src/integration/evaluate_agent.py
@@ -8,3 +8,24 @@ def evaluate(model_path, episodes=5):
     env = FlappyEnv(render=True)
     agent = Agent()
     agent.load(model_path)
+    for ep in range(episodes):
+        
+        state = env.reset()
+        done = False
+        info = {}
+        while not done:
+            action = agent.act(state, eval_mode=True)
+            state, reward, done, info = env.step(action)
+            env.render()
+        print('Episode', ep, 'score', info.get('score'))
+    env.close()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', required=True)
+    parser.add_argument('--episodes', type=int, default=5)
+    args = parser.parse_args()
+    evaluate(args.model, episodes=args.episodes)
+
